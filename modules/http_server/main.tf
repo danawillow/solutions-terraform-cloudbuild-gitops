231334a01fcbd496d60d1d757a111dfea6292609
Dana Hoffman <danawillowhoffman@gmail.com>
2020-03-31 11:25:29 -0700

    Add label (#3)
    * Add label
    
    * Update main.tf

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -41,4 +41,8 @@ resource "google_compute_instance" "http_server" {
 
   # Apply the firewall rule to allow external IPs to access this instance
   tags = ["http-server"]
+  
+  labels = {
+    environment = "${local.network}"
+  }
 }
